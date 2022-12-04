d3712d3da7f9f24aa0ed52d41609536be76f92db
Haja Asmath <asmath1997@gmail.com>
2022-12-05 01:59:38 +0400

    Added readme and small change on header

--- src/components/Header/DropDown/LoginDropDown.js
+++ src/components/Header/DropDown/LoginDropDown.js
@@ -3,16 +3,20 @@ import { Link } from "react-router-dom";
 import { useAuth } from "../../../Context/AuthProvider";
 import './LoginDropDown.css'
 
-export default function LogInDropDown({credits}) {
+export default function LogInDropDown({credits, setDropdown}) {
 
     const auth = useAuth();
 
     const handleLogout = () => {
         auth.logOut();
     }
+
+    const handleClick = () => {
+        setDropdown(false);
+    }
     return <Box className='account-dropdown'>
         <Typography variant='span'>Available Credits: {credits} Credits</Typography>
-        <Link to='/profile'>Account Overview</Link>
+        <Link onClick={handleClick} to='/profile'>Account Overview</Link>
         <Link to='/' onClick={handleLogout}>Logout</Link>
     </Box>
 }
