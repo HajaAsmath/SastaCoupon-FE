7569f8ab093e3eda321581c0543b9157b758242f
Milind Chaudhari <milind220397@gmail.com>
2022-12-06 22:48:50 +0530

    Profile and History Changes

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -61,6 +61,7 @@ function Profile() {
         address_id: profile.ADDRESS_ID,
       })
       .then(() => {
+        alert("Saved Successfully");
       });
   };
   const handleHistory = async () => {
@@ -74,6 +75,7 @@ function Profile() {
 
   useEffect(() => {
     const { userId } = JSON.parse(localStorage.getItem(SESSION_STORAGE_KEY));
+    console.log(userId);
     axios
       .get(fullUrl, {
         params: {
--- src/constants/Constants.js
+++ src/constants/Constants.js
@@ -1,2 +1,3 @@
 export const SESSION_STORAGE_KEY = "sasta-coupon-app";
 export const BACKEND_BASE_URL = "https://sastacoupon.cyclic.app";
+
